e949c5c30964399819a4004ffcf1836ed418f3a9
Peggy Lin <stu90233@gmail.com>
2023-12-23 13:45:11 +0800

    deploy

---
+++ assets/AboutView-ab499b3d.js
@@ -0,0 +1,1 @@
+import{_ as e,o,a as t}from"./index-17317806.js";const s={};function a(c,n,r,p,_,i){return o(),t("h1",null,"This is About page.")}const f=e(s,[["render",a]]);export{f as default};
---
+++ assets/HomeView-57c5966e.js
@@ -0,0 +1,1 @@
+import{_ as e,o,a as s}from"./index-17317806.js";const t={components:{}},c={class:"home"};function n(a,r,_,p,i,m){return o(),s("div",c," 這是首頁 ")}const f=e(t,[["render",n]]);export{f as default};
