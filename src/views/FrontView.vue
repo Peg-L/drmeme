1e375666447da85012d7a1f25ae0f4bd9777d3a6
Peggy Lin <stu90233@gmail.com>
2024-01-24 00:10:06 +0800

    refactor: 💡 refactor footer RWD

--- src/views/FrontView.vue
+++ src/views/FrontView.vue
@@ -61,19 +61,23 @@
   </nav>
   <router-view></router-view>
   <footer class="footer bg-dark py-14">
-    <div class="container d-flex justify-content-between align-items-center">
+    <div
+      class="container d-flex flex-column flex-md-row justify-content-between align-items-center"
+    >
       <div>
         <h1>
           <router-link to="/" class="navbar-brand d-block fs-2"
             >Dr. Meme</router-link
           >
         </h1>
-        <p class="text-secondary-700 fs-sm mt-1">
+        <p class="text-secondary-700 fs-sm mt-1 d-none d-md-block">
           Copyright © 2020 Dr.Meme All Rights Reserved.
         </p>
       </div>
-      <div class="d-flex flex-column align-items-end">
-        <ul class="d-flex gap-5 list-unstyled">
+      <div
+        class="d-flex flex-column align-items-center align-items-md-end mt-7 mt-md-0 container"
+      >
+        <ul class="d-flex flex-column flex-md-row gap-5 list-unstyled">
           <li>
             <router-link
               to="/articles"
@@ -106,37 +110,37 @@
             >
           </li>
         </ul>
-        <ul class="mt-10 list-unstyled d-flex gap-10">
-          <li>
+        <ul class="mt-5 list-unstyled row gx-10 gy-5 justify-content-center">
+          <li class="col-5 col-md-3">
             <a
-              class="text-light text-center d-flex flex-column align-items-center w-56px hover-primary-500"
+              class="text-light text-center d-flex flex-column align-items-center hover-primary-500"
               href="https://podcasts.apple.com/tw/podcast/dr-meme/id1522408497"
               target="_blank"
               ><i class="fa-solid fa-podcast fs-2"></i>
               <div class="mt-1">Apple Podcasts</div></a
             >
           </li>
-          <li>
+          <li class="col-5 col-md-3">
             <a
-              class="text-light text-center d-flex flex-column align-items-center w-56px hover-primary-500"
+              class="text-light text-center d-flex flex-column align-items-center hover-primary-500"
               href="https://open.spotify.com/show/4gm4BSePy7Xoi08Nd8KH87"
               target="_blank"
               ><i class="fa-brands fa-spotify fs-2"></i
               ><span class="mt-1">Spotify</span></a
             >
           </li>
-          <li>
+          <li class="col-5 col-md-3">
             <a
-              class="text-light text-center d-flex flex-column align-items-center w-56px hover-primary-500"
+              class="text-light text-center d-flex flex-column align-items-center hover-primary-500"
               href="https://www.instagram.com/memedical_slashie/"
               target="_blank"
               ><i class="fa-brands fa-instagram fs-2"></i>
               <span class="mt-1 text-nowrap">斜槓人生</span></a
             >
           </li>
-          <li>
+          <li class="col-5 col-md-3">
             <a
-              class="text-light text-center d-flex flex-column align-items-center w-56px hover-primary-500"
+              class="text-light text-center d-flex flex-column align-items-center hover-primary-500"
               href="https://www.instagram.com/doctormeme_tw/"
               target="_blank"
               ><i class="fa-brands fa-instagram fs-2"></i
@@ -145,6 +149,9 @@
           </li>
         </ul>
       </div>
+      <p class="text-secondary-700 fs-sm mt-10 mt-md-1 d-md-none">
+        Copyright © 2020 Dr.Meme All Rights Reserved.
+      </p>
     </div>
   </footer>
 </template>
