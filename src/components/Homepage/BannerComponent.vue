f143538a458cb4e3f3ce7c97d60b237130559b6a
Peggy Lin <stu90233@gmail.com>
2024-03-09 15:32:29 +0800

    refactor: 💡 extract components & add podcast function & fix rwd

---
+++ src/components/Homepage/BannerComponent.vue
@@ -0,0 +1,334 @@
+<template>
+  <!-- 首頁 banner 區塊 -->
+  <div class="bg-secondary-100 position-relative overflow-hidden">
+    <div class="container">
+      <div class="row justify-content-between">
+        <!-- banner 內容區塊 -->
+        <div
+          class="order-2 order-md-1 col-md-5 d-flex flex-column justify-content-between pt-8 pb-4 pt-md-30 pb-md-20"
+        >
+          <h1
+            class="display-1 fw-bold fst-italic text-primary-500 position-relative"
+          >
+            <span class="banner-title banner-title-1">Dr. Meme</span><br /><span
+              class="banner-title banner-title-2"
+              >的斜槓人生</span
+            >
+            <img
+              src="../../assets/images/banner/cursor.png"
+              alt="箭頭"
+              class="position-absolute bottom-n15 end-10"
+            />
+          </h1>
+          <div class="d-none d-md-block mt-md-16">
+            <div class="mb-3 fs-4">
+              <div class="d-inline-block">
+                <span>嗨~ 我是 Dr. Meme，</span>
+              </div>
+              <div class="d-inline-block d-md-block d-lg-inline-block">
+                <div ref="typewriteAnimated" class="typewrite-animation">
+                  <span
+                    ref="typewriteText"
+                    class="d-inline-block border-bottom border-4 border-primary-500"
+                    >{{ typedText }}</span
+                  >
+                </div>
+              </div>
+            </div>
+            <button
+              type="button"
+              class="py-4 px-15 fs-4 btn-dark border-0 button-hover"
+            >
+              斜槓故事
+            </button>
+          </div>
+        </div>
+        <!-- 首頁 banner 右側主圖  -->
+        <div
+          class="order-1 order-md-2 col-md-6 col-lg-7 d-flex justify-content-center align-items-center position-relative pt-8 pb-4 pt-md-30 pb-md-20"
+        >
+          <!-- banner 聲波圓 動畫 -->
+          <div class="circle circle-1"></div>
+          <div class="circle circle-2"></div>
+          <div class="circle circle-3"></div>
+          <div class="circle circle-4"></div>
+
+          <div class="z-3">
+            <img
+              src="../../assets/images/banner/drmeme.png"
+              alt="dr.meme"
+              class="banner-img-home"
+            />
+          </div>
+          <div class="position-absolute top-50 start-50 translate-middle z-2">
+            <img
+              src="../../assets/images/banner/meme1.png"
+              alt="meme1"
+              class="meme-img meme-img-1"
+            />
+            <img
+              src="../../assets/images/banner/meme2.png"
+              alt="meme2"
+              class="meme-img meme-img-2"
+            />
+            <img
+              src="../../assets/images/banner/meme3.png"
+              alt="meme3"
+              class="meme-img meme-img-3"
+            />
+            <img
+              src="../../assets/images/banner/meme4.png"
+              alt="meme4"
+              class="meme-img meme-img-4"
+            />
+            <img
+              src="../../assets/images/banner/meme5.png"
+              alt="meme5"
+              class="meme-img meme-img-5"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="container mt-6 mt-md-0 d-flex justify-content-center">
+    <div class="row text-center d-md-none">
+      <div>
+        <div class="mb-3 fs-6">
+          <div class="d-inline-block">
+            <span>嗨~ 我是 Dr. Meme，</span>
+          </div>
+          <div class="d-inline-block">
+            <div ref="typewriteAnimated" class="typewrite-animation">
+              <span
+                ref="typewriteText"
+                class="d-inline-block border-bottom border-4 border-primary-500"
+                >{{ typedText }}</span
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <button
+          type="button"
+          class="py-4 px-15 fs-4 btn-dark border-0 button-hover"
+        >
+          斜槓故事
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      // arr 陣列儲存要變化的文字內容
+      arr: [
+        '是一位醫生',
+        '是潛水教練',
+        '是楓樹種子',
+        '經營 IG 迷因帳',
+        '有錄 Podcast',
+      ],
+      typedText: '',
+      currentIndex: 0,
+      typingSpeed: 100, // 調整打字速度 (毫秒)
+      pauseDuration: 3000, // 停頓時間 (毫秒)
+    };
+  },
+  methods: {
+    typeText() {
+      if (this.currentIndex < this.arr.length) {
+        const currentElement = this.arr[this.currentIndex];
+        if (this.typedText.length < currentElement.length) {
+          this.typedText += currentElement.charAt(this.typedText.length);
+        } else {
+          setTimeout(() => {
+            // Move to the next element in the array
+            this.currentIndex++;
+            // Clear typedText for the next element
+            this.typedText = '';
+            // Call typeText again for the next iteration
+            this.typeText();
+          }, this.pauseDuration);
+          return;
+        }
+      } else {
+        // 所有項目跑完後重置
+        this.currentIndex = 0;
+      }
+      setTimeout(this.typeText, this.typingSpeed);
+    },
+  },
+  mounted() {
+    this.typeText();
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/SCSS/all.scss';
+
+h1 {
+  a {
+    font-family: 'Iceberg';
+  }
+}
+
+.banner-title {
+  position: relative;
+  white-space: nowrap;
+  &::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    border-bottom: 4px dashed $secondary-500;
+    padding-bottom: 10px;
+  }
+}
+
+.banner-title-1::after {
+  width: 105%;
+}
+
+.banner-title-2::after {
+  width: 105%;
+}
+
+// banner 右側主圖
+.banner-img-home {
+  padding: 5px;
+  position: relative;
+  width: 263px;
+
+  @include media-breakpoint-up(sm) {
+    width: 350px;
+  }
+
+  @include media-breakpoint-up(lg) {
+    width: 486px;
+  }
+}
+
+.meme-img {
+  opacity: 0.3;
+  position: absolute;
+
+  // 安妮亞
+  &-1 {
+    bottom: 65px;
+    right: 73px;
+    width: 63px;
+
+    @include media-breakpoint-up(md) {
+      bottom: 123px;
+      right: 144px;
+      width: 117px;
+    }
+  }
+
+  // 蜘蛛人
+  &-2 {
+    bottom: 103px;
+    left: 52px;
+    width: 80px;
+
+    @include media-breakpoint-up(md) {
+      bottom: 206px;
+      left: 105px;
+      width: 148px;
+    }
+  }
+
+  // 大腦
+  &-3 {
+    top: 90px;
+    left: 60px;
+    width: 72px;
+
+    @include media-breakpoint-up(md) {
+      top: 180px;
+      left: 120px;
+      width: 133px;
+    }
+  }
+
+  // oh no
+  &-4 {
+    top: 55px;
+    right: 101px;
+    width: 126px;
+
+    @include media-breakpoint-up(md) {
+      display: none;
+      top: 110px;
+      right: 202px;
+      width: 200px;
+    }
+
+    @include media-breakpoint-up(lg) {
+      display: block;
+    }
+  }
+
+  // 貓打貓
+  &-5 {
+    bottom: 7px;
+    left: 115px;
+    width: 75px;
+
+    @include media-breakpoint-up(md) {
+      bottom: 14px;
+      left: 234px;
+      width: 156px;
+    }
+  }
+}
+
+.circle {
+  position: absolute;
+  border-radius: 50%;
+  width: 370px;
+  height: 370px;
+  opacity: 0;
+  animation: scaleIn 4s infinite cubic-bezier(0.36, 0.11, 0.89, 0.32);
+
+  &-1 {
+    background-color: rgba($primary-500, 0.5);
+    animation-delay: -3s;
+  }
+
+  &-2 {
+    background-color: rgba($secondary-600, 0.5);
+    animation-delay: -2s;
+  }
+
+  &-3 {
+    background-color: rgba($primary-300, 0.5);
+    animation-delay: -1s;
+  }
+
+  &-4 {
+    background-color: rgba($secondary-600, 0.5);
+    animation-delay: 0s;
+  }
+}
+
+@keyframes scaleIn {
+  0% {
+    opacity: 0;
+    transform: scale(0.5, 0.5);
+  }
+  50% {
+    opacity: 0.5;
+  }
+  100% {
+    transform: scale(2, 2);
+    opacity: 0;
+  }
+}
+</style>
---
+++ src/components/Homepage/FollowIgComponent.vue
@@ -0,0 +1,275 @@
+<template>
+  <!-- 首頁 追蹤 IG 區塊 -->
+  <div class="container pt-20 pt-md-30 d-flex flex-column align-items-center">
+    <!-- 追蹤 IG 標題 -->
+    <TitleBlockComponent
+      title="INSTAGRAM"
+      text1="一天一迷因"
+      text2="醫生遠離我"
+    />
+
+    <!-- 追蹤 IG 追蹤區塊 -->
+    <div
+      class="w-100 w-md-auto border border-4 border-dark d-flex flex-column flex-md-row justify-content-between align-items-center p-6 p-md-8 bdrs-4"
+    >
+      <div
+        class="d-flex flex-column flex-md-row justify-content-md-between align-items-center mb-6 mb-md-0 me-md-10"
+      >
+        <img
+          src="../../assets/images/followIG/ig-avatar.png"
+          alt="drmeme 頭像"
+          class="w-115px mb-4 mb-md-0 me-md-5"
+        />
+        <div class="text-center text-md-start">
+          <p class="fs-4 fw-bold mb-1 mb-md-2">@doctormeme_tw</p>
+          <p>983 則貼文 <span class="ms-4">100K 粉絲</span></p>
+        </div>
+      </div>
+      <div>
+        <a
+          href="https://www.instagram.com/doctormeme_tw"
+          target="_blank"
+          class="btn w-100 py-4 px-15 bg-light border border-4 border-dark bdrs-4 fs-5 fs-md-4 fw-bold button-hover"
+          >追蹤 IG</a
+        >
+      </div>
+    </div>
+  </div>
+  <!-- 追蹤 IG 貼文區塊 -->
+  <div class="bg-dark bg-md-transparent py-3 py-md-0 mt-4 mt-md-20">
+    <swiper-container
+      class="w-100 bg-posts container posts-swiper"
+      :space-between="spaceBetween"
+      :pagination="false"
+      :navigation="false"
+      :breakpoints="{
+        375: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        992: {
+          slidesPerView: 3,
+        },
+        1200: {
+          slidesPerView: 4,
+        },
+      }"
+      @swiperprogress="onProgress"
+      @swiperslidechange="onSlideChange"
+    >
+      <swiper-slide>
+        <div class="slide-img slide-img-1 mb-4"></div>
+        <div
+          class="d-flex justify-content-between text-light fw-bold text-light"
+        >
+          <div>
+            <i class="fa-solid fa-heart text-primary-500 me-1"></i> 5,679
+          </div>
+          <div>202/03/06</div>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="slide-img slide-img-2 mb-4"></div>
+        <div class="d-flex justify-content-between text-light fw-bold">
+          <div>
+            <i class="fa-solid fa-heart text-primary-500 me-1"></i> 11.6K
+          </div>
+          <div>2023/03/01</div>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="slide-img slide-img-3 mb-4"></div>
+        <div class="d-flex justify-content-between text-light fw-bold">
+          <div>
+            <i class="fa-solid fa-heart text-primary-500 me-1"></i> 7,526
+          </div>
+          <div>2023/02/02</div>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="slide-img slide-img-4 mb-4"></div>
+        <div class="d-flex justify-content-between text-light fw-bold">
+          <div>
+            <i class="fa-solid fa-heart text-primary-500 me-1"></i> 1,427
+          </div>
+          <div>2023/12/01</div>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="slide-img slide-img-5 mb-4"></div>
+        <div class="d-flex justify-content-between text-light fw-bold">
+          <div>
+            <i class="fa-solid fa-heart text-primary-500 me-1"></i> 2,499
+          </div>
+          <div>2023/02/02</div>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="slide-img slide-img-6 mb-4"></div>
+        <div class="d-flex justify-content-between text-light fw-bold">
+          <div>
+            <i class="fa-solid fa-heart text-primary-500 me-1"></i> 3,663
+          </div>
+          <div>2023/05/11</div>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="slide-img slide-img-7 mb-4"></div>
+        <div class="d-flex justify-content-between text-light fw-bold">
+          <div>
+            <i class="fa-solid fa-heart text-primary-500 me-1"></i> 5,981
+          </div>
+          <div>2023/01/30</div>
+        </div>
+      </swiper-slide>
+      <swiper-slide>
+        <div class="slide-img slide-img-8 mb-4"></div>
+        <div class="d-flex justify-content-between text-light fw-bold">
+          <div>
+            <i class="fa-solid fa-heart text-primary-500 me-1"></i> 31.8K
+          </div>
+          <div>2022/12/25</div>
+        </div>
+      </swiper-slide>
+    </swiper-container>
+  </div>
+  <!-- 貼文切換按鈕 -->
+  <div
+    class="navigation position-relative mt-5 mt-md-11 d-flex justify-content-center align-items-start gap-5"
+  >
+    <div
+      class="swiper-button-prev"
+      type="button"
+      @click="postsSlidePrev('.posts-swiper')"
+    >
+      <span class="material-symbols-outlined fs-1"> arrow_circle_left </span>
+    </div>
+    <div
+      class="swiper-button-next"
+      type="button"
+      @click="postsSlideNext('.posts-swiper')"
+    >
+      <span class="material-symbols-outlined fs-1"> arrow_circle_right </span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { register } from 'swiper/element/bundle';
+import TitleBlockComponent from '@/components/Homepage/TitleBlockComponent.vue';
+
+export default {
+  components: {
+    TitleBlockComponent,
+  },
+  data() {
+    return {
+      spaceBetween: 24,
+    };
+  },
+  beforeMount() {
+    register();
+  },
+  methods: {
+    postsSlidePrev(el) {
+      const swiperEl = document.querySelector(el);
+      swiperEl.swiper.slidePrev();
+    },
+    postsSlideNext(el) {
+      const swiperEl = document.querySelector(el);
+      swiperEl.swiper.slideNext();
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/SCSS/all.scss';
+
+// 追蹤 ig 貼文
+.slide-img {
+  height: 410px;
+  @include media-breakpoint-up(md) {
+    height: 357px;
+  }
+  transition: filter 0.3s ease-in-out;
+  cursor: pointer;
+  border-radius: 4px;
+
+  @include media-breakpoint-up(md) {
+    filter: brightness(70%) blur(2px);
+  }
+
+  &:hover {
+    filter: none;
+  }
+
+  &-1 {
+    background: url('../../assets/images/posts/post1.jpg') no-repeat center /
+      cover;
+  }
+
+  &-2 {
+    background: url('../../assets/images/posts/post2.jpg') no-repeat center /
+      cover;
+  }
+
+  &-3 {
+    background: url('../../assets/images/posts/post3.jpg') no-repeat center /
+      cover;
+  }
+
+  &-4 {
+    background: url('../../assets/images/posts/post4.jpg') no-repeat center /
+      cover;
+  }
+
+  &-5 {
+    background: url('../../assets/images/posts/post5.jpg') no-repeat center /
+      cover;
+  }
+
+  &-6 {
+    background: url('../../assets/images/posts/post6.jpg') no-repeat center /
+      cover;
+  }
+
+  &-7 {
+    background: url('../../assets/images/posts/post7.jpg') no-repeat center /
+      cover;
+  }
+
+  &-8 {
+    background: url('../../assets/images/posts/post8.jpg') no-repeat center /
+      cover;
+  }
+}
+
+.bg-posts::after {
+  content: '';
+  position: absolute;
+  background-color: $dark;
+
+  @include media-breakpoint-up(md) {
+    bottom: -24px;
+    left: -24px;
+    width: calc(100% + 24px * 2);
+    height: 50%;
+    background: linear-gradient(
+      360deg,
+      rgba($dark, 0.9) 10%,
+      rgba($dark, 0.5) 50%,
+      rgba($dark, 0) 100%
+    );
+  }
+}
+
+// 切換貼文按鈕
+.swiper-button-prev,
+.swiper-button-next {
+  line-height: 0;
+  user-select: none;
+}
+</style>
