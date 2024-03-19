500df705a9f429b036c12148c5861fb35053b9d6
Peggy Lin <stu90233@gmail.com>
2024-03-19 10:08:38 +0800

    Updates

---
+++ assets/AdminHome-71602a98.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./index-c34ecdd2.js";const r={};function n(o,c){return"admin dashboard home"}const _=e(r,[["render",n]]);export{_ as default};
---
+++ assets/getDate-a6a896d2.js
@@ -0,0 +1,1 @@
+const r={methods:{getDate(e){const t=new Date;t.setTime(e);const a=t.getFullYear(),o=t.getMonth()+1,n=t.getDate();return`${a}/${o}/${n}`}}};export{r as g};
